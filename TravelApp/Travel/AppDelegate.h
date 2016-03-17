4028bc8690f3523b22eb53a91b4df752ded698d4
tangbentian <tanngbt@163.com>
2016-03-17 17:37:13 +0800

    add

---
+++ TravelApp/Travel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Travel
+//
+//  Created by tarena002 on 16/3/17.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravelApp/Travel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Travel
+//
+//  Created by tarena002 on 16/3/17.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
